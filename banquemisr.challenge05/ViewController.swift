51180f5dbd163639de943f9b12320fcdd0751e67
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-27 00:09:45 +0300

    First commit

---
+++ banquemisr.challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banquemisr.challenge05
+//
+//  Created by zyad Baset on 27/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
