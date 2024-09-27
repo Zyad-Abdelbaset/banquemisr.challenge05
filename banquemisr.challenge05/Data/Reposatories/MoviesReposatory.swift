818218f851b4767afa0d280b02d634f5c1a4fd59
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-27 05:54:34 +0300

    adding movieRepository

--- banquemisr.challenge05/Data/Reposatories/MoviesReposatory.swift
+++ banquemisr.challenge05/Data/Reposatories/MoviesReposatory.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+protocol MovieRepository {
+    func getMovies(endPoint:MovieListEndPoints,completion: @escaping (Result<[Movie], Error>) -> Void)
+}
+
+class MovieRepositoryImpl: MovieRepository {
+    private let api: MovieAPI
+    
+    init(api: MovieAPI) {
+        self.api = api
+    }
+    
+    func getMovies(endPoint:MovieListEndPoints,completion: @escaping (Result<[Movie], Error>) -> Void) {
+        api.fetchMovies(endPoint:endPoint ) { result in
+            switch result {
+            case .success(let movies):
+                // Convert DTOs (from the API) to Domain Models
+                completion(.success(movies))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
