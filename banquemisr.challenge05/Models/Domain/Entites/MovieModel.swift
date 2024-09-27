ffcda01c8583239c4034439d2ac30ec7c8e71447
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-27 05:19:38 +0300

    creating domain entity movie

--- banquemisr.challenge05/Models/Domain/Entites/MovieModel.swift
+++ banquemisr.challenge05/Models/Domain/Entites/MovieModel.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+// MARK: - MoviesResponse
+struct MoviesResponse: Codable {
+    let movies: [Movie]
+    
+    enum CodingKeys: String,CodingKey {
+        case movies = "results"
+    }
+}
+
+// MARK: - Result
+struct Movie: Codable {
+    let adult: Bool
+    let backdropPath: String?
+    let id: Int
+    let originalLanguage: String?
+    let originalTitle, overview: String
+    let popularity: Double
+    let posterPath, releaseDate, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
--- banquemisr.challenge05/Service/NetworkService.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkService.swift
-//  banquemisr.challenge05
-//
-//  Created by zyad Baset on 27/09/2024.
-//
-
-import Foundation
