f14ca18f40afdd7e7c386f237878a54d5f701081
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-27 05:53:44 +0300

    adding usecase of fetching movies

--- banquemisr.challenge05/Models/Domain/UseCases/FetchMoviesUseCase.swift
+++ banquemisr.challenge05/Models/Domain/UseCases/FetchMoviesUseCase.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+protocol FetchMoviesUseCase {
+    func execute(endPoint:MovieListEndPoints,completion: @escaping (Result<[Movie],Error>) -> Void)
+}
+class FetchMoviesUseCaseImpl: FetchMoviesUseCase {
+    private let repository: MovieRepository
+    
+    init(repository: MovieRepository) {
+        self.repository = repository
+    }
+    
+    func execute(endPoint:MovieListEndPoints,completion: @escaping (Result<[Movie], Error>) -> Void) {
+        repository.getMovies(endPoint: endPoint) { result in
+            // Additional domain logic can be added here if needed
+            completion(result)
+        }
+    }
+}
