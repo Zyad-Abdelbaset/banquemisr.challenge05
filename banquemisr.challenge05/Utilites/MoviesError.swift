8771f79b15bdcce639155fc7fcaf5f27c210d99d
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-27 05:17:59 +0300

    adding error handler type enumeration

--- banquemisr.challenge05/Utilites/MoviesError.swift
+++ banquemisr.challenge05/Utilites/MoviesError.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+enum MovieError: Error, CustomNSError {
+    case invalidResponse
+    case invalidEndPoint
+    case apiError
+    case noData
+    case serializationError
+    case noConnection
+    var localizedDescription: String {
+        switch self {
+        case .invalidResponse: return "Invalid response code"
+        case .noData: return "there is No data (Empty data)"
+        case .serializationError: return "Failed in decoding data"
+        case .invalidEndPoint: return "Not a valid End Point"
+        case .apiError: return "Problem in connecting with API"
+        case .noConnection: return "the internet connection maybe lost or weak"
+        }
+    }
+    var errorUserInfo: [String: Any] {
+        [NSLocalizedDescriptionKey: localizedDescription]
+    }
+}
+
+
