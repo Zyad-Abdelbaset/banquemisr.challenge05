ad560e52cb36bdbffffda52e7d6b7f05842d1606
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-28 22:02:10 +0300

    adding use case to fetch details of movie

---
+++ banquemisr.challenge05/Domain/UseCases/FetchMovieDetailsUseCase.swift
@@ -0,0 +1,25 @@
+//
+//  FetchMovieDetailsUseCase.swift
+//  banquemisr.challenge05
+//
+//  Created by zyad Baset on 28/09/2024.
+//
+
+import Foundation
+protocol FetchMovieDetailsUseCase {
+    func execute(MovieId:String,completion: @escaping (Result<MovieDetailsEntity,MovieError>,Bool) -> Void)
+}
+class FetchMovieDetailsUseCaseImpl: FetchMovieDetailsUseCase {
+    private let repository: MovieRepository
+    
+    init(repository: MovieRepository) {
+        self.repository = repository
+    }
+    
+    func execute(MovieId:String,completion: @escaping (Result<MovieDetailsEntity, MovieError>,Bool) -> Void) {
+        repository.getMovieDetails(movieId: MovieId){ result,connectionFlag in
+            // Additional domain logic can be added here if needed
+            completion(result,connectionFlag)
+        }
+    }
+}
