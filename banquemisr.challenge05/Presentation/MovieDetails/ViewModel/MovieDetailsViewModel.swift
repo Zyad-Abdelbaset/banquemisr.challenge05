bc25aa8d25cb0f4bce3cb1c9b46764f089ff4e19
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-28 23:51:47 +0300

    adding movieDetailsViewModel

--- banquemisr.challenge05/Presentation/MovieDetails/ViewModel/MovieDetailsViewModel.swift
+++ banquemisr.challenge05/Presentation/MovieDetails/ViewModel/MovieDetailsViewModel.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+class MovieDetailsViewModel{
+    var model:MovieDetailsEntity!
+    private let fetchMovieDetailsUseCase:FetchMovieDetailsUseCase
+    var onlineFlag:String = "Checking"
+    var noResult : ((String)->Void) = {_ in }
+    var putData : (()->Void) = {}
+    var movieId:String
+    
+    init(movieId:String){
+        self.movieId = movieId
+        fetchMovieDetailsUseCase = FetchMovieDetailsUseCaseImpl(repository: MovieRepositoryImpl())
+        fetchMovieDetails()
+    }
+    
+    func fetchMovieDetails(){
+        fetchMovieDetailsUseCase.execute(MovieId: movieId) {[weak self] result, flag in
+            switch result{
+            case .success(let movie):DispatchQueue.main.async{
+                self?.model = movie
+                self?.putData()
+                print("Succed in fetching data")
+            }
+            case .failure(let error):
+                DispatchQueue.main.async{
+                    self?.noResult(error.localizedDescription)
+                    print(error.localizedDescription)
+                    print("Error in fetching data")
+                }
+            }
+            
+        }
+    }
+    
+}
