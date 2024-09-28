a917a7cf60ee759a99bbc38f4daa88e4c0a81cd9
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-28 20:53:37 +0300

    adding function to checnetwork so choose from where retrive data

--- banquemisr.challenge05/Data/Reposatories/MoviesReposatory.swift
+++ banquemisr.challenge05/Data/Reposatories/MoviesReposatory.swift
@@ -7,25 +7,61 @@
 
 import Foundation
 protocol MovieRepository {
-    func getMovies(endPoint:MovieListEndPoints,completion: @escaping (Result<[Movie], Error>) -> Void)
+    func getMovies(endPoint:MovieListEndPoints,completion: @escaping (Result<[MoviesList], MovieError>,Bool) -> Void)
+    func getMovieImage(imgPath:String,completion: @escaping (Result<Data, MovieError>) -> Void)
 }
 
 class MovieRepositoryImpl: MovieRepository {
     private let api: MovieAPI
-    
-    init(api: MovieAPI) {
-        self.api = api
+    private let coreDataManager: CoreDataManager
+    private let networkChecker: ConnectionProtocol
+    init() {
+        self.api = MovieAPI.shared
+        coreDataManager = CoreDataManager.shared
+        networkChecker = Connection.shared
     }
     
-    func getMovies(endPoint:MovieListEndPoints,completion: @escaping (Result<[Movie], Error>) -> Void) {
-        api.fetchMovies(endPoint:endPoint ) { result in
-            switch result {
-            case .success(let movies):
-                // Convert DTOs (from the API) to Domain Models
-                completion(.success(movies))
-            case .failure(let error):
-                completion(.failure(error))
+    func getMovies(endPoint:MovieListEndPoints,completion: @escaping (Result<[MoviesList], MovieError>,Bool) -> Void) {
+        networkChecker.checkConnectivity { connectionFlag in
+            if connectionFlag {
+                self.api.fetchMovies(endPoint:endPoint ) { result in
+                    switch result {
+                    case .success(let movies):
+                        completion(.success(movies),true)
+                        //let serialqueue = DispatchQueue(label: "handling coredata")
+                        DispatchQueue.main.async {
+                                //self.coreDataManager.deleteAllMovies(endPoint: endPoint)
+                                self.coreDataManager.addingMoviesToDB(arrMovies: movies, endPoint: endPoint)
+                        }
+                    case .failure(let error):
+                        completion(.failure(error),true)
+                    }
+                }
+            }else{
+                completion(.success(self.coreDataManager.getMovies(endPoint: endPoint)),false)
             }
         }
+        
+    }
+    
+    func getMovieImage(imgPath:String,completion: @escaping (Result<Data, MovieError>) -> Void){
+        networkChecker.checkConnectivity { connectionFlag in
+            if connectionFlag{
+                self.api.fetchMovieImage(imgPath: imgPath) { result in
+                    switch result{
+                    case .success(let imgData): completion(.success(imgData))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                }
+            }else{
+                if let data = Data(base64Encoded: imgPath,options: .ignoreUnknownCharacters){
+                    completion(.success(data))
+                }else{
+                    completion(.failure(.invalidResponse))
+                }
+            }
+        }
+        
     }
 }
