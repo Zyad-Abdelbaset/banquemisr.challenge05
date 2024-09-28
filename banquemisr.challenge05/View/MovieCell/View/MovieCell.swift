2f34acf6f3bbe0d0e57947720cdf75f489926c9f
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-28 20:50:13 +0300

    adding nib file to cell of movie

--- banquemisr.challenge05/View/MovieCell/View/MovieCell.swift
+++ banquemisr.challenge05/View/MovieCell/View/MovieCell.swift
@@ -8,25 +8,39 @@
 import UIKit
 
 class MovieCell: UITableViewCell {
-
+    
+    @IBOutlet weak var lblVoteCount: UILabel!
+    @IBOutlet weak var lblAvgVote: UILabel!
     @IBOutlet weak var lblDate: UILabel!
     @IBOutlet weak var lblTitle: UILabel!
     
-    
+    var viewModel:MovieCellViewModel!
     @IBOutlet weak var imgView: UIImageView!
-    var model:Movie!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     func putData() {
-        lblTitle.text=model.title
-        lblDate.text=model.releaseDate
+        imgView.layer.cornerRadius=20
+        lblTitle.text=viewModel.model.title
+        lblDate.text=viewModel.model.releaseDate
+        viewModel.getMovieImage(completion: { result in
+            switch result{
+            case .success(let data): DispatchQueue.main.async {
+                self.imgView.image = UIImage(data: data)
+            }
+            case .failure(_):DispatchQueue.main.async {
+                self.imgView.image = UIImage(named: "defaultImage")
+            }
+                
+            }
+        })
+        
     }
-    
 }
