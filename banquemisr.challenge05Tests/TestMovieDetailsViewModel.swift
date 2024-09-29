95c2002c3800c2d8c869f4f384f82d6a2ab05b36
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-29 17:58:03 +0300

    test MovieDetailsViewModel

--- banquemisr.challenge05Tests/TestMovieDetailsViewModel.swift
+++ banquemisr.challenge05Tests/TestMovieDetailsViewModel.swift
@@ -6,30 +6,46 @@
 //
 
 import XCTest
-
+@testable import banquemisr_challenge05
 final class TestMovieDetailsViewModel: XCTestCase {
-
+    var viewModel:MovieDetailsViewModel!
     override func setUpWithError() throws {
+        viewModel = MovieDetailsViewModel(movieId: "957452")
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    func testFetchSpecificMovie(){
+        let opt = OperationQueue()
+        let exp = expectation(description: "Waiting view model finish hitting network")
+        let block1 = BlockOperation {
+            self.viewModel.fetchMovieDetails()
+            exp.fulfill()
+        }
+        var block2 = BlockOperation()
+        block2.addDependency(block1)
+        opt.addOperations([block1,block2], waitUntilFinished: false)
+        XCTAssertEqual(viewModel.onlineFlag, "Checking")
+        waitForExpectations(timeout: 3)
+        block2 = BlockOperation {
+           XCTAssertEqual(self.viewModel.model!.title, "The Crow")
+       }
+    }
+    
+    func testFetchImage(){
+        let exp = expectation(description: "Waiting Fetching image")
+        viewModel.getMovieImage { result in
+            switch result {
+            case .success(let imgData): XCTAssertNotNil(imgData);exp.fulfill()
+            case .failure(let error): XCTFail(error.localizedDescription);exp.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 3)
+    }
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
 }
