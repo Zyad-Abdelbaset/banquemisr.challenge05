f6ffa2af4cad765c203c54eeac7716652402c0e9
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-29 17:58:35 +0300

    test network

--- banquemisr.challenge05Tests/TestingNetwork.swift
+++ banquemisr.challenge05Tests/TestingNetwork.swift
@@ -6,24 +6,41 @@
 //
 
 import XCTest
-
+@testable import banquemisr_challenge05
 final class TestingNetwork: XCTestCase {
 
+    var movieAPI:MovieAPI?
     override func setUpWithError() throws {
+        movieAPI = MovieAPI.shared
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
+        movieAPI = nil
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testFetchSpecificMovie(){
+        let myExpectation = expectation(description: "wait api")
+        movieAPI!.fetchMovies(endPoint: "933260", model: MovieDetailsEntity.self) { result in
+            switch result{
+            case .success(let data): XCTAssertNotNil(data, "Nil inside data");myExpectation.fulfill();XCTAssertEqual(data.title, "The Substance")
+            case .failure(let error): XCTFail("Faild to fetch data\(error.localizedDescription)")
+            }
+        }
+        waitForExpectations(timeout: 2)
+        
     }
+    func testFetchingImage(){
+        let myExpectation = expectation(description: "wait api")
+        movieAPI!.fetchMovieImage(imgPath: "/Asg2UUwipAdE87MxtJy7SQo08XI.jpg") { result in
+            switch result{
+            case .success(let data ): XCTAssertNotNil(data, "Nil inside data");myExpectation.fulfill()
+            case .failure(let error): XCTFail("Faild to fetch data\(error.localizedDescription)")
+            }
+        }
+        waitForExpectations(timeout: 2)
+    }
+
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
