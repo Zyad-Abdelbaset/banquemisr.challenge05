62dfeadd8d056c9a8259ccadc7dbcfe71f9ab916
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-09-29 17:56:59 +0300

    adding test to core data

--- banquemisr.challenge05Tests/TestCoreData.swift
+++ banquemisr.challenge05Tests/TestCoreData.swift
@@ -6,23 +6,53 @@
 //
 
 import XCTest
-
+import CoreData
+@testable import banquemisr_challenge05
 final class TestCoreData: XCTestCase {
 
+    var coreData:CoreDataManager!
+    
     override func setUpWithError() throws {
+        coreData = CoreDataManager.shared
+        clearCache()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testFetchingMovieList(){
+        let arr = coreData.getMovies(endPoint: .nowPlaying)
+        XCTAssertNotEqual(arr.count, 0, "Empty core data")
+        guard let obj = arr.first else {XCTAssertNil(arr.first) ;return}
+        let obj2 = coreData.getSpecificMovieDetails(MovieId: "\(obj.id)")
+        if obj2.count != 0 {
+            XCTAssertEqual(obj.id, obj2.first!.id, "Fetching data correct")
+        }
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testSavingSpecificMovie() {
+        XCTAssertNotEqual(coreData.getSpecificMovieDetails(MovieId: "957452").count, 1)
+        let movie = MovieDetailsEntity(adult: false, backdropPath: "/Asg2UUwipAdE87MxtJy7SQo08XI.jpg", budget: 20000, genres: [], id: 957452, originalLanguage: "en", overview: "Good movie", releaseDate: "2023", runtime: 125, status: "released", title: "The Crow")
+        coreData.addingMovieDetailsToDB(movieDetail: movie)
+        //XCTAssertNotEqual(coreData.getSpecificMovieDetails(MovieId: "957452").count, 0)
+        //XCTAssertEqual(coreData.getSpecificMovieDetails(MovieId: "957452").first?.title, "The Crow")
+    }
+    func clearCache() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName:"MovieDetailsModel")
+        do {
+            let movies = try managedContext.fetch(fetchRequest)
+            for movie in movies {
+                managedContext.delete(movie)
+            }
+            try managedContext.save()
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    override func tearDownWithError() throws {
+        coreData = nil
+        clearCache()
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testPerformanceExample() throws {
